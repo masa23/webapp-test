f97cb5bdd5f218375e866b3418d63b9b9514bee8
Masa23 <masa23@gmail.com>
2025-06-11 11:45:01 +0900

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -35,9 +35,8 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
-	ws: true,
+        ws: true,
       },
     }
   }
-
 })
